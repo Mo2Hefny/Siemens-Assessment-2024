a59ed99d8752651a82daf274959ead568b36e675
Mo2Hefny <moamensaied1@gmail.com>
2024-07-12 23:33:24 +0300

    feat: add integer validation

--- Assignment_2/src/main.cpp
+++ Assignment_2/src/main.cpp
@@ -1,24 +1,56 @@
 #include "packet.h"
+#include <sstream>
+
+/**
+ * readInteger - Reads an integer or rounds a float from the console and throws an error if
+ * invalid input was given then tries again.
+ */
+int readInteger() {
+  int integer;
+  bool failed;
+  do
+  {
+    string input;
+    cin >> input;
+    istringstream iss(input);
+    failed = !(iss >> integer);
+    try
+    {
+      if (failed)
+        throw invalid_argument("Input is not an integer. Try again.");
+    }
+    catch (const invalid_argument &e)
+    {
+      cerr << "Caught invalid_argument: " << e.what() << endl;
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+  } while (failed);
+
+  return integer;
+}
 
 int main()
 {
   int packetsNum;
   cout << "Enter number of packets: ";
-  cin >> packetsNum;
+  packetsNum = readInteger();
 
   PacketValidator validator;
 
   vector<int> packetModules(packetsNum);
   cout << "Enter module numbers for each packet:" << endl;
-  for (auto &moduleNumber : packetModules)
-    cin >> moduleNumber;
+  for (auto& moduleNumber : packetModules)
+    moduleNumber = readInteger();
 
-  try {
+  try
+  {
     validator.setPacketModules(packetModules);
     validator.validatePackets();
     validator.saveValidation();
   }
-  catch (const invalid_argument& e) {
+  catch (const invalid_argument &e)
+  {
     cerr << "Caught invalid_argument: " << e.what() << endl;
   }
 
