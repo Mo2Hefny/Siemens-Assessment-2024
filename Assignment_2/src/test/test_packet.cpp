ae1b74db1e8553e60488918c29239d0e1c50e208
Mo2Hefny <moamensaied1@gmail.com>
2024-07-12 23:12:58 +0300

    test: add unit testing to packet.h

--- Assignment_2/src/test/test_packet.cpp
+++ Assignment_2/src/test/test_packet.cpp
@@ -1,32 +1,119 @@
 #include <gtest/gtest.h>
 #include "../packet.h"
 
-TEST(PacketValidatorTest, ValidatePackets) {
+TEST(PacketValidatorTest, AccumulativeValues) {
   PacketValidator validator;
-  std::vector<int> modules = {1, 2, 3, 4, 5};
+  vector<int> modules = {1, 2, 3, 4, 5};
 
   validator.setPacketModules(modules);
   validator.validatePackets();
 
-  std::vector<bool> validation = validator.getValidation();
+  vector<bool> validation = validator.getValidation();
 
   ASSERT_EQ(validation.size(), modules.size());
-  EXPECT_TRUE(validation[0]);
-  EXPECT_TRUE(validation[1]);
+  for (auto& result: validation)
+    EXPECT_TRUE(result);
 }
 
-TEST(PacketValidatorTest, ValidatePackets2) {
+TEST(PacketValidatorTest, EqualValues) {
   PacketValidator validator;
-  std::vector<int> modules = {1, 2, 3, 4, 5};
+  vector<int> modules = {1, 1, 1, 1, 1};
 
   validator.setPacketModules(modules);
   validator.validatePackets();
 
-  std::vector<bool> validation = validator.getValidation();
+  vector<bool> validation = validator.getValidation();
 
   ASSERT_EQ(validation.size(), modules.size());
-  EXPECT_TRUE(validation[0]);
-  EXPECT_TRUE(validation[1]);
+  for (auto& result: validation)
+    EXPECT_TRUE(result);
+}
+
+TEST(PacketValidatorTest, NegativeValues) {
+  PacketValidator validator;
+  vector<int> modules = {-1, -2, -3, -4, -5};
+
+  EXPECT_THROW(validator.setPacketModules(modules), invalid_argument);
+  validator.validatePackets();
+
+  vector<bool> validation = validator.getValidation();
+
+  EXPECT_TRUE(validation.empty());
+}
+
+TEST(PacketValidatorTest, MixValues) {
+  PacketValidator validator;
+  vector<int> modules = {6, -2, 2, -1, 7};
+
+  EXPECT_THROW(validator.setPacketModules(modules), invalid_argument);
+  validator.validatePackets();
+
+  vector<bool> validation = validator.getValidation();
+
+  EXPECT_TRUE(validation.empty());
+}
+
+TEST(PacketValidatorTest, ZeroValues) {
+  PacketValidator validator;
+  vector<int> modules = {0, 0, 0, 0, 0};
+
+  EXPECT_THROW(validator.setPacketModules(modules), invalid_argument);
+  validator.validatePackets();
+
+  vector<bool> validation = validator.getValidation();
+
+  EXPECT_TRUE(validation.empty());
+}
+
+TEST(PacketValidatorTest, UnorderedPositiveValues) {
+  PacketValidator validator;
+  vector<int> modules = {6, 1, 2, 3, 7};
+
+  validator.setPacketModules(modules);
+  validator.validatePackets();
+
+  vector<bool> validation = validator.getValidation();
+
+  EXPECT_EQ(validation.size(), modules.size());
+  EXPECT_EQ(validation, vector<bool> ({true, false, true, true, false}));
+}
+
+TEST(PacketValidatorTest, UnorderedPositiveValuesWithDups) {
+  PacketValidator validator;
+  vector<int> modules = {4, 4, 2, 1, 1};
+
+  validator.setPacketModules(modules);
+  validator.validatePackets();
+
+  vector<bool> validation = validator.getValidation();
+
+  EXPECT_EQ(validation.size(), modules.size());
+  EXPECT_EQ(validation, vector<bool> ({true, true, false, false, true}));
+}
+
+TEST(PacketValidatorTest, FullCycleVector) {
+  PacketValidator validator;
+  vector<int> modules = {4, 5, 6, 1, 1};
+
+  validator.setPacketModules(modules);
+  validator.validatePackets();
+
+  vector<bool> validation = validator.getValidation();
+
+  EXPECT_EQ(validation.size(), modules.size());
+  for (auto& result: validation)
+    EXPECT_TRUE(result);
+}
+
+TEST(PacketValidatorTest, EmptyVector) {
+  PacketValidator validator;
+  vector<int> modules;
+
+  EXPECT_THROW(validator.setPacketModules(modules), invalid_argument);
+
+  vector<bool> validation = validator.getValidation();
+
+  EXPECT_TRUE(validation.empty());
 }
 
 int main(int argc, char **argv) {
