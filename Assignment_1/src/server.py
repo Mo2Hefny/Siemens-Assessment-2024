9f5e86a52cfb83c8ccc25d40ae53c57ea4714964
Mo2Hefny <moamensaied1@gmail.com>
2024-07-13 13:21:55 +0300

    test: add setup test for Assignment 1

--- Assignment_1/src/server.py
+++ Assignment_1/src/server.py
@@ -3,16 +3,28 @@ from itertools import repeat
 
 class Server:
   """
+  Server class simulates a configurable server with options and validation states.
   """
-  # Static variable to store server instance
-  server = None
 
   def __init__(self, system_option_size: int) -> None:
+    if system_option_size <= 0:
+      raise ValueError("Number of system options must be positive")
+    
     self.options_size = system_option_size
     self.options = list(repeat(True, system_option_size))
     self.valid_tc = True
 
   def configure(self, options: list, is_master: bool) -> None:
+    """
+    Configures the server with a list of options from the client.
+    
+    Args:
+        options (list): List of options from a client to configure the server with.
+        is_master (bool): Boolean flag indicating if the client is the master.
+    
+    Raises:
+        ValueError: If the length of options does not match the expected options size.
+    """
     if len(options) != self.options_size:
       raise ValueError("You must configure the Server with the same number of options")
     if is_master:
